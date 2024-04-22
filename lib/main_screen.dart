646ee7ad99aafa0a5586b5ce36b78b58221e7ab7
Maksim Lin <maks@manichord.com>
2024-04-22 20:52:35 +1000

    tweak layout

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -138,9 +138,9 @@ class _MainScreenState extends State<MainScreen> {
           // hardcode position to align with background image given hardcoded window
           // size in my_application.cc for Linux
           Positioned(
-            left: 86,
-            top: 75,
-            width: 320 * 1.4,
+            left: 70,
+            top: 70,
+            width: 320 * 1.5,
             height: 240 * 1.5,
             child: PicoScreen(
               _grid,

c920f869c6a071a7a8418cfc445afa45aab9b9fc
Maksim Lin <maks@manichord.com>
2024-12-06 21:54:50 +1100

    fix invert background for whitespace char bug

--- lib/widgets/screen_char_row.dart
+++ lib/widgets/screen_char_row.dart
@@ -14,18 +14,26 @@ class ScreenCharRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: rowChars
-          .map((cell) => Text(
-                cell.char,
-                style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                      letterSpacing: 2.2,
-                      fontSize: 13,
-                      color: cell.invert ? grid.background : cell.color,
-                      backgroundColor:
-                          cell.invert ? cell.color : Colors.transparent,
-                    ),
-              ))
-          .toList(),
+      children: rowChars.map((cell) {
+        bool isInvertedSpaceChar = cell.char == " " && cell.invert;
+        return Text(
+          // need this ugly hack due to open Flutter bug:
+          // https://github.com/flutter/flutter/issues/112766
+          isInvertedSpaceChar ? "\u2588" : cell.char,
+          style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                height: 1,
+                letterSpacing: 2.2,
+                fontSize: 13,
+                color: isInvertedSpaceChar
+                    ? cell.color
+                    : (cell.invert ? grid.background : cell.color),
+                backgroundColor: isInvertedSpaceChar
+                    ? cell.color
+                    : (cell.invert ? cell.color : grid.background),
+                decoration: TextDecoration.none,
+              ),
+        );
+      }).toList(),
     );
   }
 }

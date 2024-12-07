515c4d3ec5e89a201b20eefd9348b3750c84060d
Maksim Lin <maks@manichord.com>
2024-12-07 13:44:39 +1100

    fix inverted background for space chars
    this required a ugly hack to workaround bug in Flutter SDK

--- lib/widgets/screen_char_row.dart
+++ lib/widgets/screen_char_row.dart
@@ -14,16 +14,18 @@ class ScreenCharRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisSize: MainAxisSize.min,
       children: rowChars.map((cell) {
         bool isInvertedSpaceChar = cell.char == " " && cell.invert;
         return Text(
           // need this ugly hack due to open Flutter bug:
           // https://github.com/flutter/flutter/issues/112766
           isInvertedSpaceChar ? "\u2588" : cell.char,
+          textAlign: TextAlign.right,
           style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                height: 1,
-                letterSpacing: 2.2,
-                fontSize: 13,
+                height: 1.2,
+                letterSpacing: 1,
+                fontSize: 14,
                 color: isInvertedSpaceChar
                     ? cell.color
                     : (cell.invert ? grid.background : cell.color),

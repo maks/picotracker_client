fb1877e1bb077af2ff9ce15e259e886da2578cf2
Maksim Lin <maks@manichord.com>
2024-12-07 15:22:18 +1100

    reinstate working linux build

--- lib/serialport_native.dart
+++ lib/serialport_native.dart
@@ -1,17 +1,26 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:udev/udev.dart';
+
 import 'package:flutter_libserialport/flutter_libserialport.dart';
 import 'command_builder.dart';
 
 class SerialPortHandler {
   final CmdBuilder cmdBuilder;
   SerialPort? port;
+  StreamSubscription? subscription;
+  StreamSubscription? usbUdevStream;
+
+  String portname = "/dev/ttyACM0";
 
   SerialPortHandler(this.cmdBuilder);
 
   void chooseSerialDevice() async {
     //TODO, for now just use hardcoded port
     try {
-      String linuxUsbTTY = "/dev/ttyACM0";
-      SerialPort(linuxUsbTTY).close();
+      SerialPort(portname).close();
+      _initPort();
+      _listenForUsbDevices();
     } catch (e) {
       // ignore: avoid_print
       print("could not close serial port $e");
@@ -25,5 +34,47 @@ class SerialPortHandler {
     for (int i = 0; i < byteBuf.lengthInBytes; i++) {
       cmdBuilder.addByte(byteBuf[i]);
     }
+  }
+
+  void _initPort() {
+    print("Init Port");
+    try {
+      port = SerialPort(portname);
+      port!.openReadWrite();
+      _listenPort();
+    } catch (_) {}
+  }
+
+  Future<void> _listenPort() async {
+    if (port == null) {
+      print("null port can't listen");
+      return;
+    }
+    SerialPortReader reader = SerialPortReader(port!, timeout: 20);
+
+    subscription = reader.stream.listen((data) {
+      final byteBuf = data.buffer.asByteData();
+      for (int i = 0; i < byteBuf.lengthInBytes; i++) {
+        cmdBuilder.addByte(byteBuf.getUint8(i));
+      }
+    });
+  }
+
+  void _listenForUsbDevices() {
+    final context = UdevContext();
+    final stream = context.monitorDevices(subsystems: ['usb']);
+    usbUdevStream = stream.listen((d) {
+      // print("USBevent: $d");
+      // d.properties.forEach((key, value) => print("$key:$value"));
+      final ttyFile = File(portname);
+      if (ttyFile.existsSync() && port == null) {
+        // print("found port file, so now do init");
+        _initPort();
+      } else {
+        // print("no more port, do close");
+        port?.close();
+        port = null;
+      }
+    });
   }
 }

73e5c8f396eaa01e5b37719ece76f51b60efb4a8
Maksim Lin <maks@manichord.com>
2024-12-24 17:58:07 +1100

    request screen refresh after connecting

--- lib/serialport_web.dart
+++ lib/serialport_web.dart
@@ -41,6 +41,17 @@ class SerialPortHandler {
     // Listen to data coming from the serial device.
     final reader = port?.readable?.getReader() as ReadableStreamDefaultReader;
 
+    if (port != null) {
+      debugPrint("port opened: ${port.readable}");
+      // request full screen refresh after opening
+      final request = Uint8List.fromList([REMOTE_COMMAND_MARKER, 0x02]);
+      final JSUint8Array jsReq = request.toJS;
+      final writer = port.writable?.getWriter();
+      writer?.write(jsReq);
+      // Allow the serial port to be closed later.
+      writer?.releaseLock();
+    }
+
     while (true) {
       final result = await reader.read().toDart;
       if (result.done) {
